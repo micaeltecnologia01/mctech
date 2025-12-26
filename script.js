1a1c9b62693d2c8ef3726eefa219bc67e2f83fc8
micaeltecnologia01 <micael.tecnologia@gmail.com>
2025-12-26 15:21:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('mouseenter', () => {
+    card.style.boxShadow = '0 0 20px #38bdf8';
+  });
+
+  card.addEventListener('mouseleave', () => {
+    card.style.boxShadow = 'none';
+  });
+});
